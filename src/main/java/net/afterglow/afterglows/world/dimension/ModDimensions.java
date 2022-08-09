94da4682c02d2fd29bbe9767aa65e5bf0ff0d110
Afterglow79 <cakeby11@gmail.com>
2022-08-09 14:38:52 -0400

    The thing

--- src/main/java/net/afterglow/afterglows/world/dimension/ModDimensions.java
+++ src/main/java/net/afterglow/afterglows/world/dimension/ModDimensions.java
@@ -1,7 +1,10 @@
 package net.afterglow.afterglows.world.dimension;
 
 import net.afterglow.afterglows.AfterglowsMod;
+import net.afterglow.afterglows.item.ModItems;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -18,6 +21,13 @@ public class ModDimensions {
 
     public static void register() {
         AfterglowsMod.LOGGER.debug("Registering ModDimensions for " + AfterglowsMod.MOD_ID);
+
+        CustomPortalBuilder.beginPortal()
+                .frameBlock(Blocks.DIAMOND_BLOCK)
+                .destDimID(HF_DIMENSION_KEY.getValue())
+                .tintColor(102, 102, 102)
+                .lightWithItem(ModItems.HIGH_FREQUENCY_BLADE)
+                .registerPortal();
 
     }
 }
